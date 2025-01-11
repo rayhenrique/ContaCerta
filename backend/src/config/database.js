e3601dd66926669310f66fafaf58228f1853457d
Ray Henrique <rayhenrique@gmail.com>
2025-01-11 19:45:05 -0300

    fix: Ajusta configuração do Sequelize para usar IPv4

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -2,27 +2,39 @@ require('dotenv').config();
 
 module.exports = {
   development: {
+    dialect: 'mysql',
+    host: '127.0.0.1',
     username: process.env.DB_USER || 'root',
     password: process.env.DB_PASS || '',
     database: process.env.DB_NAME || 'contacerta',
-    host: process.env.DB_HOST || 'localhost',
-    dialect: 'mysql',
+    define: {
+      timestamps: true,
+      underscored: true,
+    },
     logging: false
   },
   test: {
+    dialect: 'mysql',
+    host: '127.0.0.1',
     username: process.env.DB_USER || 'root',
     password: process.env.DB_PASS || '',
     database: process.env.DB_NAME || 'contacerta_test',
-    host: process.env.DB_HOST || 'localhost',
-    dialect: 'mysql',
+    define: {
+      timestamps: true,
+      underscored: true,
+    },
     logging: false
   },
   production: {
+    dialect: 'mysql',
+    host: '127.0.0.1',
     username: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    dialect: 'mysql',
+    define: {
+      timestamps: true,
+      underscored: true,
+    },
     logging: false
   }
 };
