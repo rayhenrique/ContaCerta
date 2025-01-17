e69314ccb1050f55e6bfe6da000216591883f2d2
Ray Henrique <rayhenrique@gmail.com>
2025-01-17 14:12:39 -0300

    Configurar Nginx, atualizar seed de usuário e preparar deploy

--- backend/src/database/seeders/20250110000001-admin-user.js
+++ backend/src/database/seeders/20250110000001-admin-user.js
@@ -4,11 +4,10 @@ const bcrypt = require('bcryptjs');
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    const hashedPassword = await bcrypt.hash('1508rcrc', 10);
     await queryInterface.bulkInsert('Users', [{
       name: 'Ray Henrique',
       email: 'rayhenrique@gmail.com',
-      password: hashedPassword,
+      password: '1508rcrc',
       access_level: 'admin',
       created_at: new Date(),
       updated_at: new Date()
